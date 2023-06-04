aed9a869b2a29371bf2e9c05d8e5fded22686dd6
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-06-04 23:05:41 +0700

    try commit

--- resources/views/livewire/table/table-riwayat-pengajuan-cuti-user.blade.php
+++ resources/views/livewire/table/table-riwayat-pengajuan-cuti-user.blade.php
@@ -93,7 +93,7 @@
                             <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 {{ $item->durasi }} Hari
                             </td>
-                            <td class="px-6 py-4 line-clamp-1 truncate font-medium text-gray-900 whitespace-nowrap">
+                            <td class="px-6 py-4 line-clamp-2 truncate font-medium text-gray-900 whitespace-nowrap">
                                 {{ $item->alasan }}
                             </td>
                             <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
