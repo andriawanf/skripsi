854cbe3f40150ce7774fc4411df74027b3d40df8
Andriawan Firmansyah <andriawnfirmansyah@gmail.com>
2023-06-09 20:42:34 +0700

    perbaikan notif icon

--- app/Http/Livewire/Layout/Navbar.php
+++ app/Http/Livewire/Layout/Navbar.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Livewire\Layout;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Navbar extends Component
 {
+    public $unreadNotificationsCount;
+    public $notifications;
+
+    public function mount()
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $this->unreadNotificationsCount = $user->unreadNotifications->count();
+            $this->notifications = $user->notifications()->whereNull('read_at')->latest()->limit(5)->get();
+        }
+    }
     public function render()
     {
         return view('livewire.layout.navbar');
+    }
+
+    public function markAllAsRead()
+    {
+        if (Auth::check()) {
+            Auth::user()->unreadNotifications->markAsRead();
+            $this->unreadNotificationsCount = 0;
+        }
+    }
+
+    public function markAsRead($notificationId)
+    {
+        $notification = Auth::user()->notifications->find($notificationId);
+
+        if ($notification) {
+            $notification->markAsRead();
+            $this->unreadNotificationsCount--;
+            $this->notifications = Auth::user()->notifications()->whereNull('read_at')->latest()->limit(5)->get();
+        }
     }
 }
--- resources/views/livewire/layout/navbar.blade.php
+++ resources/views/livewire/layout/navbar.blade.php
@@ -11,20 +11,72 @@
     </button>
 
     <div class="flex flex-row justify-center items-center space-x-3 p-2 mt-2 sm:hidden">
-        <i class='bx bxs-bell bx-sm text-gray-900'></i>
-        <img src="{{ url('images/foto.jpg') }}" alt="foto profile" class="w-10 h-10 rounded-full object-cover"
-            data-popover-target="popover-user-profile">
-        <div data-popover id="popover-user-profile" role="tooltip"
-            class="absolute z-10 invisible inline-block w-36 font-poppins font-medium text-sm text-gray-900 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
+        {{-- notifikasi --}}
+        <button type="button" class="relative inline-flex items-center text-sm font-medium text-center text-white mr-2"
+            data-popover-target="data-notifikasi">
+            <i class='bx bxs-bell bx-sm text-gray-900'></i>
+            <span class="sr-only">Notifications</span>
+            <div
+                class="absolute inline-flex items-center justify-center w-5 h-5 text-[10px] font-bold text-white bg-red-600 border-2 border-white rounded-full -top-2 -right-2 dark:border-gray-900">
+                @if ($unreadNotificationsCount > 0)
+                    <span class="badge">{{ $unreadNotificationsCount }}</span>
+                @else
+                    <span class="badge">0</span>
+                @endif
+            </div>
+        </button>
+        <div data-popover id="data-notifikasi" role="tooltip"
+            class="absolute z-10 invisible w-96 font-poppins font-medium text-sm text-gray-900 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-lg opacity-0 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
             <div class="p-3">
-                <ul class="space-y-3">
-                    <li>Profile</li>
-                    <li>Pengaturan</li>
-                    <li>Keluar</li>
+                <ul class="space-y-4">
+                    @forelse ($notifications as $notification)
+                        <div class="flex flex-row space-x-2 justify-between items-baseline">
+                            <li class="notification border-b border-gray-300 pb-2 w-full">
+                                {{ $notification->data['message'] }}</li>
+                            <button wire:click="markAsRead('{{ $notification->id }}')"
+                                class="px-3 py-2 w-fit bg-red-600 hover:bg-red-800 rounded-lg text-medium text-white">Baca</button>
+                        </div>
+                    @empty
+                        <li class="notification border-b border-gray-300 pb-2 w-full text-center">Tidak ada notifikasi
+                            baru untuk anda</li>
+                    @endforelse
+
                 </ul>
             </div>
             <div data-popper-arrow></div>
         </div>
+        {{--  --}}
+
+        {{-- foto profil user --}}
+        @if (auth()->user()->foto == 'images/logo.png')
+                <img src="{{ url('/' . auth()->user()->foto) }}" alt="foto profile"
+                    class="w-10 h-10 rounded-full object-cover" data-popover-target="foto-profil-user">
+            @else
+                <img src="{{ asset('storage/foto-profil/' . auth()->user()->foto) }}" alt="foto profile"
+                    class="w-10 h-10 rounded-full object-cover" data-popover-target="foto-profil-user">
+            @endif
+            <div data-popover id="foto-profil-user" role="tooltip"
+                class="absolute z-10 invisible inline-block w-44 font-poppins font-medium text-sm text-gray-900 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-lg opacity-0 dark:text-gray-400 dark:bg-gray-800 dark:border-gray-600">
+                <div class="p-3">
+                    <ul class="space-y-4">
+                        <li>
+                            <a href="{{ route('pengaturan') }}" class="text-gray-900 rounded-lg hover:bg-gray-100">
+                                <span class="">Pengaturan profil</span>
+                            </a>
+                        </li>
+                        <li>
+                            <div class="w-full h-[1px] bg-gray-300"></div>
+                        </li>
+                        <li>
+                            <form action="{{ route('logout') }}" method="GET">
+                                <button
+                                    class="px-3 py-2 w-full bg-red-600 hover:bg-red-800 rounded-lg text-medium text-white">Keluar</button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+                <div data-popper-arrow></div>
+            </div>
     </div>
 </div>
 
